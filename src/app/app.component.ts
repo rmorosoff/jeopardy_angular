65e41f6b43a25090cf767567b6abb7c956c756d6
Rudy Morosoff <rudy.morosoff@libertymutual.com>
2017-09-18 15:42:31 -0700

    Initial commit with working functionality.  Still need to split into 2 components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JeopardyService } from './jeopardy.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Angular Jeopardy Lite';
+
+  questionInfo;
+
+  totalScore: number = 0;
+
+  userAnswer;
+
+  constructor(private jeopardyService : JeopardyService){}
+
+  getDataFromService(){
+    this.jeopardyService.getQuestionInfo()
+    .subscribe(
+      questionInfo => {
+        this.questionInfo = questionInfo[0];
+        console.log(this.questionInfo)
+      }
+    )
+  }
+  ngOnInit (){
+    this.getDataFromService();
+  }
+  
+
+  checkAnswer(givenAnswer) {
+    console.log(this.userAnswer);
+    console.log(this.questionInfo.answer);
+    if (this.userAnswer == this.questionInfo.answer) {
+      this.totalScore = this.totalScore + this.questionInfo.value
+    } else{
+      console.log("You blew it!!")
+    }
+
+    this.getDataFromService();
+
+    this.userAnswer = '';
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { JeopardyService } from './jeopardy.service';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    JeopardyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
